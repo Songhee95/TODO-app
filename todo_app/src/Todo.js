46fb21b652c186ec81d7e7d90aa83a96e0c5764d
songhee <thdgml5537@gmail.com>
2020-12-10 17:19:04 -0500

    edit button with modal added

--- todo_app/src/Todo.js
+++ todo_app/src/Todo.js
@@ -1,21 +1,37 @@
-import { Button, List, ListItem, ListItemAvatar, ListItemText } from '@material-ui/core';
+import { Button, List, ListItem, ListItemAvatar, ListItemText, Modal } from '@material-ui/core';
 import './Todo.css';
-import React from 'react';
+import React, { useState } from 'react';
 import db from "./firebase";
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 
 //props stands for properties
 function Todo(props) {
+    const [open, setOpen] = useState(false);
+    const handleOpen = ()=>{
+        setOpen(true);
+    }
     return (
+        <>
+        <Modal
+            open={open}
+            onClose={e =>setOpen(false)}
+        >
+            <div>
+                <h1>I am a modal</h1>
+                <button onClick={e=> setOpen(false)}></button>
+            </div>
+        </Modal>
+
         <List className="todo__list">
             <ListItem>
                 <ListItemAvatar>
-
                 </ListItemAvatar>
                 <ListItemText primary={props.todo.todo} secondary="Dummy deadline ⏰"/>
             </ListItem>
+            <button onClick={e => setOpen(true)}>Edit</button>
             <DeleteForeverIcon onClick={event => db.collection('todos').doc(props.todo.id).delete()}/>
         </List>
+        </>
     )
 }
 
